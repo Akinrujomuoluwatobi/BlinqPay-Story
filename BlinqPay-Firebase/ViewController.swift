ec26851f4864741a03c22a81941c88d264686be0
Oluwatobiloba Akinrujomu <tobiak2011@gmail.com>
2022-02-10 15:42:21 +0100

    Initial Commit

---
+++ BlinqPay-Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BlinqPay-Firebase
+//
+//  Created by Oluwatobiloba Akinrujomu on 10/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ BlinqPay-FirebaseTests/BlinqPay_FirebaseTests.swift
@@ -0,0 +1,33 @@
+//
+//  BlinqPay_FirebaseTests.swift
+//  BlinqPay-FirebaseTests
+//
+//  Created by Oluwatobiloba Akinrujomu on 10/02/2022.
+//
+
+import XCTest
+@testable import BlinqPay_Firebase
+
+class BlinqPay_FirebaseTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
